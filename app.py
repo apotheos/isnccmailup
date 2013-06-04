fa28d4a2d55b561bb3a9a16b54e845b4b385ca18
Mike Holler <apotheos121@gmail.com>
2013-06-04 15:50:50 -0500

    modified config

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 __author__ = 'mjholler'
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 import nccmail
 from database import Database
 
+
 @app.route("/")
 def index():
     db = Database()
-    output = ""
-    if nccmail.isup():
-        output += "<h1>Yes. NCC mail appears to be working just fine</h1>"
-    else:
-        output += "<h1>Nope. NCC mail looks down from here! :("
-    output += "<br />Uptime: %s percent" % db.get_mail_uptime()
-    return output
+    return render_template('index.html', isup=nccmail.isup(), uptime=db.get_mail_uptime())
 
 if __name__ == "__main__":
     app.run()

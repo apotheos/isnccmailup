138e52726ef07dbb031198178f699f67d9094ce6
Mike Holler <apotheos121@gmail.com>
2013-06-05 14:24:03 -0500

    Fixed datetimes.

--- app.py
+++ app.py
@@ -9,19 +9,26 @@ config = json.load(open(os.path.join(basedir, 'config.json'), 'r+'))
 from flask import Flask, render_template
 app = Flask(__name__)
 
+from dateutil import tz
+
 import helper
 from database import Database
-
+from datetime import datetime
+import time
 
 
 @app.route("/")
 def index():
+    to_zone = tz.gettz(config['application']['timezone'])
     db = Database()
     kwargs = dict()
     kwargs['mail_up'], kwargs['sample_time'] = db.get_last_sample()
+    # print type(kwargs['sample_time'])
+    kwargs['sample_time'] = datetime.fromtimestamp(time.mktime(kwargs['sample_time'].timetuple()), tz=to_zone)
+    # print kwargs['sample_time']
     kwargs['sample_time'] = kwargs['sample_time'].strftime('%B %d, %Y at %I:%M %p')
     kwargs['webmail_url'] = config['email']['webmail_url']
-    kwargs['mail_uptime']=db.get_mail_uptime()
+    kwargs['mail_uptime'] = db.get_mail_uptime()
     kwargs['mail_level'] = helper.get_level(kwargs['mail_uptime'])
     kwargs['site_url']= config['site']['url']
     kwargs['site_uptime'] = db.get_main_site_uptime()
